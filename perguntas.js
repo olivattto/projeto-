352dbcc2a51e48284ac9489ef4a49a764b898282
olivattto <00001092722464sp@al.educacao.sp.gov.br>
2024-11-08 14:15:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Português',
+    'Quais são as 3 fases do romantismo?',
+    'indianismo, ultrarromantismo e geração social'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz de 144?',
+    'A raiz de 144 é 12'
+)
